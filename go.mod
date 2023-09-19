d8da76d46f0f8fa4f1b3c9d2efc4739261a43409
Abdul Fatah <fatahhamidah@gmail.com>
2023-09-19 21:40:10 +0700

    initial commit for go-module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/abdulfatahhamidah/go-say-hello
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,8 @@
+package go_module_say_hello
+
+import "fmt"
+
+func SayHello() string {
+    fmt.Println("Say Hello From Go-Module")
+    return "say Hello return value"
+}
