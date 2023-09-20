96f1bfe229a8daa9b3fea4ca21b76c0eabf6165d
Abdul Fatah <fatahhamidah@gmail.com>
2023-09-20 11:16:06 +0700

    add introduction function

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package go_module_say_hello
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func SayHello() string {
-    fmt.Println("Say Hello From Go-Module")
-    return "say Hello return value"
+	fmt.Println("Say Hello From Go-Module")
+	return "say Hello return value"
+}
+
+// add new feature to module, Introduction function representing information
+// about my personal
+type Person struct {
+	Name, Address string
+	Age           int8
+}
+
+func Introdution() string {
+	fatah := Person{"Abdul Fatah", "Bogor", 23}
+
+	return fmt.Sprintf("My name is %s, from %s, and I'm %d years old",
+		fatah.Name, fatah.Address, fatah.Age)
 }
